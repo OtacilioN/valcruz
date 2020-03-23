dd705adb7cd77271cf7684baacd37b93f9fe3363
Michael barney Junior <michaelbarneyjunior@gmail.com>
2020-03-23 10:11:10 -0300

    ordenacao dos cards por data e hotjar

--- src/screens/Protocols.js
+++ src/screens/Protocols.js
@@ -26,6 +26,26 @@ const Protocols = props => {
   }, []);
 
   const getDinamicCards = () => {
+    //order the content
+    content.sort(function(a, b) {
+      console.log(a)
+      const adate = a[1].date.split('/');
+      const bdate = b[1].date.split('/');
+  
+      //1- check year
+      if(adate[2] !== bdate[2]){
+        return bdate[2] - adate[2];
+      }
+      //2- Check Month
+      else if(adate[1] !== bdate[1]){
+        return bdate[1] - adate[1];
+      }
+      else{
+        return bdate[0] - adate[0];
+      }
+    });
+
+    //return cards
     return content.map(c => {
       const id = c[0];
       const data = c[1];
